79b6871b7e3ee7310e46005db6e42723c2c173dd
Freet8707 <freet8707@gmail.com>
2020-10-09 21:22:52 -0500

    the log in function is functioning correctly

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const db = require("../database/dbConfig");
 
 router.post('/register', async (req, res) => {
@@ -20,13 +21,43 @@ router.post('/register', async (req, res) => {
     .catch(error => {
       res.status(500).json({
         message: "There was an error adding that user",
-        error: `Error: ${error}`
+        error: error
       })
     })
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   // implement login
+  const { username, password } = req.body;
+
+  try {
+    const user = await db("users")
+      .select("*")
+      .where("users.username", username)
+      .first()
+
+    const validPassword = await bcrypt.compare(password, user.password)
+
+    if(user && validPassword) {
+      res.status(200).json({
+        message: "logged in !"
+      })
+    }
+    else {
+      res.status(401).json({
+        message: "Invalid Credentials!"
+      })
+    }
+  } catch(err) {
+    res.status(500).json({
+      message: "Error",
+      error: err
+    })
+  }
 });
+
+router.get("/cleanup", () => {
+  Users.cleanUp()
+})
 
 module.exports = router;
