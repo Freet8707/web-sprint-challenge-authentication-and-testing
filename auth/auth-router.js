c2e9ac960b10fe653a58792be2cf87d7e1e4f1ad
Freet8707 <freet8707@gmail.com>
2020-10-09 20:19:41 -0500

    implemented a register function that can add a user to the users table w/ hashed password

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,28 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
   // implement registration
+  const {username, password} = req.body;
+
+  return await db("users")
+    .insert({
+      username: username,
+      password: await bcrypt.hash(password, 14)
+    })
+    .then(response => {
+      res.status(201).json({
+        message: "user successfully created",
+        userId: response
+      })
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "There was an error adding that user",
+        error: `Error: ${error}`
+      })
+    })
 });
 
 router.post('/login', (req, res) => {
